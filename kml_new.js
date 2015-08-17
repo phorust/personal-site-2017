a142fe8c6752f7666da43941ec389cb0d6a96e13
Kevin Lee <kevinmlee.x@gmail.com>
2015-08-16 22:07:39 -0700

    add clean scrolling, command cycling

--- kml_new.js
+++ kml_new.js
@@ -1,4 +1,14 @@
+/* thanks for peeking at the source!
+ * you can find it in likely nicer form at
+ * github.com/phorust/phorust.github.io
+ */
+
+var startTime;
+
 $(document).ready(function() {
+  // for uptime
+  startTime = Date.now();
+
   $('#wrapper').addClass('loaded');
 
   /* attr selectors for href? */
