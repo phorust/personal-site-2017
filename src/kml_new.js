0eeabed7d571d07f0a03db83b3d0bd7971c3444e
Kevin Lee <kevinmlee.x@gmail.com>
2015-10-15 02:14:10 -0700

    visual tweaks: subtle links, less margin-top, no background box for code

--- src/kml_new.js
+++ src/kml_new.js
@@ -15,14 +15,12 @@ function hideAbout() {
 }
 
 function showAbout() {
-  // min-height won't actually accommodate all the code...
-  // but it will look better this way anyway.
   $('#about_content').show().addClass('loaded');
   $('#about_content > a > pre')
     .css('max-height', $('#recent_code').outerHeight(true));
   // always try to place KL in the same place, slightly above middle
   $('#content')
-    .css('min-height', Math.min($(window).height() * 0.35));
+    .css('min-height', Math.min($(window).height() * 0.60));
 }
 
 $(document).ready(function() {
