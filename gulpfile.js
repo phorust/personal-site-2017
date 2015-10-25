0ea85e5dcc364001126e1e888c903f7f5d9716da
Kevin Lee <kevinmlee.x@gmail.com>
2015-10-25 00:54:43 -0700

    add gulp livereloading

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+gulp.task('sass', _ => {
+  return gulp.src('./css/**/*.scss')
+           .pipe(sass().on('error', sass.logError))
+           .pipe(gulp.dest('./css/compiled'))
+           .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], _ => {
+    browserSync.init({
+      server: "./"
+    });
+
+    gulp.watch("./css/**/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('watch', _ => {
+  gulp.watch(['./css/**/*.scss'], ['sass']);
+});
+
+gulp.task('default', ['serve']);
