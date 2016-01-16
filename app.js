2d09d05ad9e8daeecd28ea956bd22e0467f67647
Kevin Lee <kevinmlee.x@gmail.com>
2016-01-15 22:38:50 -0800

    cosmetic changes to make boilerplate mine

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /**
- * React Static Boilerplate
- * https://github.com/koistya/react-static-boilerplate
- * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
+ * app.js
+ * main entry point
  */
 
 import 'babel/polyfill';
--- config.js
+++ config.js
@@ -1,11 +1,5 @@
-/**
- * React Static Boilerplate
- * https://github.com/koistya/react-static-boilerplate
- * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
- */
-
 export default {
-  title: 'React Static Boilerplate',
-  description: 'Static website generator for React.js web applications.',
+  title: "Kevin Lee's personal website",
+  description: "Uses @koistya's react static site boilerplate",
   googleAnalyticsId: 'UA-XXXXX-X',
 };
