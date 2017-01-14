34049ee6ccddc8f08b6651582ccde7e4819018cf
Kevin Lee <kevinmlee.x@gmail.com>
2017-01-14 00:40:19 -0800

    deletions and revert gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,27 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-var rename      = require('gulp-rename');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
-var frontMatter = require('gulp-front-matter');
-
-var messages = {
-  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
-
-gulp.task('jekyll-build', (done) => {
-  browserSync.notify(messages.jekyllBuild);
-  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-           .on('close', done);
-});
-
-gulp.task('jekyll-rebuild', ['jekyll-build'], _ => {
-  browserSync.reload();
-});
-
-gulp.task('browser-sync', ['sass', 'jekyll-build'], _ => {
-  browserSync.init({
-    server: {
-      baseDir: '_site'
-    }
-  });
-});
 
 gulp.task('sass', _ => {
-  gulp.src('./css/*.scss')
-      .pipe(frontMatter())
-      .pipe(sass({ includePaths: ['_sass']}).on('error', sass.logError))
-      // .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-      .pipe(gulp.dest('_site/css'))
-      .pipe(browserSync.stream());
-  return gulp.src('./_scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    // .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('_site/css'))
-    .pipe(browserSync.stream());
+  return gulp.src('./css/**/*.scss')
+           .pipe(sass().on('error', sass.logError))
+           .pipe(gulp.dest('./css/compiled'))
+           .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], _ => {
+    browserSync.init({
+      server: {
+        baseDir: './'
+      }
+    });
+
+    gulp.watch("./css/**/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 gulp.task('watch', _ => {
-  gulp.watch(['_sass/*.scss', 'css/*.scss'], ['sass']);
-  gulp.watch([
-    '*.html',
-    '_layouts/*.html',
-    '_includes/*.html',
-    '_posts/*.md',
-    'src/*.js'
-  ], ['jekyll-rebuild']);
+  gulp.watch(['./css/**/*.scss'], ['sass']);
 });
 
-gulp.task('build', _ => {
-  return gulp.src('index.html')
-             .pipe(rename('software.html'))
-             .pipe(gulp.dest('./'))
-             .pipe(rename('visual.html'))
-             .pipe(gulp.dest('./'))
-             .pipe(rename('index.html'))
-             .pipe(gulp.dest('./blog/'));
-});
-
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['serve']);
