52ffb2d4459993632a1d19726eeb09875435aea8
Kevin Lee <kevinmlee.x@gmail.com>
2017-01-16 02:14:41 -0800

    placeholder text

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className={styles.theOne}>
-        <span>Kevin writes, takes photos, and illustrates, sometimes, in that order. Often what he writes is code, but sometimes, it isn't. Sometimes that stuff makes it in things. Right now, he's trying to figure out how to enjoy something without worrying about being good at it. And how to be himself and who that is.</span>
+        <span>Kevin writes code for facebook. He takes photos for fun. This site is under construction.</span>
         <div className={styles.line}></div>
         <b>Heading</b>
       </div>
