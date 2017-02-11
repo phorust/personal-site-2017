f4ba16628817b5c4e4fbc79c72cdfff0546ee461
Kevin Lee <kevinmlee.x@gmail.com>
2017-02-10 18:38:57 -0800

    add fat nameplate with chinese

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ import styles from './index.scss';
 export default class App extends React.Component {
   render() {
     return (
-      <div className={styles.theOne}>
-        <span>Kevin writes code for facebook. He takes photos for fun. This site is under construction.</span>
-        <div className={styles.line}></div>
-        <b>Heading</b>
+      <div className={styles.container}>
+        <div className={styles.kevin}>Kevin</div>
+        <div className={styles.mngyuan}>梦远</div>
+        <div className={styles.lee}>Lee</div>
+        <div className={styles.theOne}>
+          <span>Kevin writes code for facebook. He takes photos for fun. This site is under construction.</span>
+          <div className={styles.line}></div>
+          <b>Heading</b>
+        </div>
       </div>
     )
   }
